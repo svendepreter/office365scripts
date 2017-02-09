907b92ee7ea4b2a8e0691ffdfa3ef9d79fc8dcb2
Sven De Preter <Sven.De.Preter@sportpaleis.net>
2017-02-09 14:05:40 +0100

    Initial Commit

---
+++ MailboxAccesPerUser/MailboxAccessPerUser.ps1
@@ -0,0 +1,6 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+Get-Mailbox | Get-MailboxPermission -User administrator@sportpaleisgroep.Be
+Remove-PSSession $Session
